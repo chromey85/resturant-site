d8340e69edf30db95dc3d58602a44a5940d069a9
Yusuf Ayyub <yayyub85@gmail.com>
2023-05-30 23:10:46 +0100

    checkpoint for the day

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {GiHamburgerMenu} from 'react-icons/gi'
-import {MdOutlineResturantMenu} from 'react-icons/md'
+import {MdOutlineRestaurantMenu} from 'react-icons/md'
 
 import images from '../../constants/images'
 import './Navbar'
@@ -19,6 +19,24 @@ const Navbar = () => (
       <li className="p__opensans"><a href="#awards" id="">Awards</a></li>
       <li className="p__opensans"><a href="#contact" id="">Contact</a></li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Register</a>
+      <div />
+      <a href="/" className="p__opensans">Book a Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={()=>{}}/>
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{}} />
+        <ul className="app__navbar-smallscreen-links">
+          <li className="p__opensans"><a href="#home" id="">Home</a></li>
+          <li className="p__opensans"><a href="#about" id="">About</a></li>
+          <li className="p__opensans"><a href="#menu" id="">Menu</a></li>
+          <li className="p__opensans"><a href="#awards" id="">Awards</a></li>
+          <li className="p__opensans"><a href="#contact" id="">Contact</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
